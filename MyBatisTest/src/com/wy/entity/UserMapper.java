9b1db71100151aecf9ea6e32117c622ee202eff2
Sinon0 <wy_sinon@126.com>
2017-10-09 17:49:34 +0800

    interface method

---
+++ MyBatisTest/src/com/wy/entity/UserMapper.java
@@ -0,0 +1,11 @@
+package com.wy.entity;
+
+import java.util.List;
+
+public interface UserMapper {
+	public User selectUser(int id);
+	public List<User> selectAll();
+	public void addUser(User user);
+	public void  updateUser(User user);
+	public void deleteUser(int id);
+}
